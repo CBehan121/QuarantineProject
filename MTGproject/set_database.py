fbf24a879c8ae838c9d1b9cc6651eee9dbe72cde
Conner Behan <conner.behan7@mail.dcu.ie>
2020-03-20 20:25:10 +0000

    fixing

--- MTGproject/set_database.py
+++ MTGproject/set_database.py
@@ -27,20 +27,23 @@ def addToDatabase():
 	  "apiKey": " AIzaSyAThe5ILc3TGvV_rIxHKxbZfQfMTtdj1jQ ",
 	  "authDomain": "deckcreator-d2ddf.firebaseapp.com",
 	  "databaseURL": "https://my-application-a29f5.firebaseio.com/",
-	  "storageBucket": "deckcreator-d2ddf.firebaseapp.com",
+	  "storageBucket": "deckcreator-d2ddf.appspot.com",
 	  "serviceAccount": "/home/conner/Documents/clubhub-270013-9a21263094f2.json" #Not putting on github
 	}
-
-
+	todaysDate = datetime.now()
+	firebase = pyrebase.initialize_app(config)
+	auth = firebase.auth()
+	user = auth.sign_in_with_email_and_password("connerbehan1@gmail.com", "") ##Not putting on github
+	db = firebase.database()
+	listOfActive = db.child("deckcreator-d2ddf").get(user['idToken']).val()
+	print(listOfActive)
 def main():
-	collectCard()
 
+	addToDatabase()
 
 
 if __name__ == '__main__':
 	main()
-
-
 
 
 		
